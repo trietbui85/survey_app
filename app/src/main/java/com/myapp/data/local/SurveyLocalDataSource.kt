0ac71400312b130b82bfb417dafa67841007c7fc
Triet Bui Minh <x3cafe@gmail.com>
2020-02-05 01:19:26 +0700

    Add SurveyLocalDataSource which is an interface to let repository interact with database

---
+++ app/src/main/java/com/myapp/data/local/SurveyLocalDataSource.kt
@@ -0,0 +1,31 @@
+package com.myapp.data.local
+
+import com.myapp.data.local.db.SurveyDatabase
+import com.myapp.data.local.db.SurveyEntity
+
+// DbDataSource is the interface to let repository interact with Database
+interface SurveyLocalDataSource {
+    // Insert list of surveys into database
+    suspend fun insertItems(items: List<SurveyEntity>)
+
+    // Get the list of surveys from database. Return empty if no data
+    suspend fun loadAll(): List<SurveyEntity>
+
+    // Delete all surveys in database
+    suspend fun deleteAll()
+}
+
+class SurveyLocalDataSourceImpl(private val database: SurveyDatabase) : SurveyLocalDataSource {
+    override suspend fun insertItems(items: List<SurveyEntity>) {
+        return database.surveyDao.insertItems(items)
+    }
+
+    override suspend fun loadAll(): List<SurveyEntity> {
+        return database.surveyDao.loadAll()
+    }
+
+    override suspend fun deleteAll() {
+        return database.surveyDao.deleteAll()
+    }
+
+}
