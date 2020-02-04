601b683378417f9be169f9cdc440ed6069878815
Triet Bui Minh <x3cafe@gmail.com>
2020-02-05 06:59:02 +0700

    Update UI of MainFragment by survey data from MainViewModel

--- app/src/main/java/com/myapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/myapp/ui/main/MainFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.myapp.R
+import com.myapp.data.repo.Result
 import com.myapp.data.repo.SurveyItem
 import com.myapp.ui.detail.DetailFragment
+import com.utils.getErrorText
 import com.utils.showToastLong
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.main_fragment.*
@@ -38,16 +41,52 @@ class MainFragment : DaggerFragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.fetchSurveys(1)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         initViewAndAction()
-
         initLiveData()
     }
 
     private fun initLiveData() {
-        viewModel.fetchSurveys(1)
+        viewModel.surveyLiveData.observe(viewLifecycleOwner, Observer {
+            when (it.status) {
+                Result.Status.LOADING -> {
+                    Timber.d("surveyLiveData: is loading")
+                    if (viewModel.isFirstTimeLoading()) {
+                        viewLoadMore.visibility = View.INVISIBLE
+                        viewLoadingFullScreen.visibility = View.VISIBLE
+                    } else {
+                        viewLoadMore.visibility = View.VISIBLE
+                        viewLoadingFullScreen.visibility = View.INVISIBLE
+                    }
+                }
+                Result.Status.ERROR -> {
+                    Timber.d("surveyLiveData: error: $it")
+                    viewLoadMore.visibility = View.INVISIBLE
+                    viewLoadingFullScreen.visibility = View.INVISIBLE
+
+                    val errorText = it.exception.getErrorText(requireContext())
+                    this.showToastLong(errorText)
+                }
+                Result.Status.SUCCESS -> {
+                    Timber.d("surveyLiveData: success: ${it.data}")
+                    viewLoadMore.visibility = View.INVISIBLE
+                    viewLoadingFullScreen.visibility = View.INVISIBLE
+                    if (it.data.isNullOrEmpty()) {
+                        this.showToastLong(R.string.no_surveys)
+                    } else {
+                        surveyAdapter.setItems(it.data.orEmpty())
+                        indicator.setViewPager(viewPager)
+                    }
+                }
+            }
+        })
     }
 
     private fun initViewAndAction() {
@@ -66,7 +105,9 @@ class MainFragment : DaggerFragment() {
                 findNavController().navigate(R.id.action_mainFragment_to_detailFragment, bundle)
             }
         })
+
         surveyAdapter.setItems(emptyList())
+
         viewPager.adapter = surveyAdapter
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
