1a189a3b03c953c1aa85210888891d0a374d179e
Triet Bui Minh <x3cafe@gmail.com>
2020-02-05 04:23:07 +0700

    Add extension method to show SnackBar

---
+++ app/src/main/java/com/myapp/utils/SnackBarExt.kt
@@ -0,0 +1,21 @@
+package com.myapp.utils
+
+import android.view.View
+import androidx.annotation.StringRes
+import com.google.android.material.snackbar.Snackbar
+
+fun View.showSnackBar(msg: String, duration: Int = Snackbar.LENGTH_SHORT) {
+    Snackbar.make(this, msg, duration).show()
+}
+
+fun View.showSnackBar(@StringRes msgId: Int, duration: Int = Snackbar.LENGTH_SHORT) {
+    Snackbar.make(this, msgId, duration).show()
+}
+
+fun View.showSnackBarLong(msg: String) {
+    Snackbar.make(this, msg, Snackbar.LENGTH_LONG).show()
+}
+
+fun View.showSnackBarLong(@StringRes msgId: Int) {
+    Snackbar.make(this, msgId, Snackbar.LENGTH_LONG).show()
+}
