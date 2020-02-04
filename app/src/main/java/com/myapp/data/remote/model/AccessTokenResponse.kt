a593ed1d1e2dfecb0055a7fa22a944860e44bf59
Triet Bui Minh <x3cafe@gmail.com>
2020-02-05 02:40:55 +0700

    Add AccessTokenResponse which is JSON response for AccessToken

---
+++ app/src/main/java/com/myapp/data/remote/model/AccessTokenResponse.kt
@@ -0,0 +1,9 @@
+package com.myapp.data.remote.model
+
+// Response Data Model
+data class AccessTokenResponse(
+    val accessToken: String = "",
+    val tokenType: String = "",
+    val expiresIn: Int = 0,
+    val createdAt: Int = 0
+)
