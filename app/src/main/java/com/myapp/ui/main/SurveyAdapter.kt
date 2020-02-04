c4b671835223fa13d751ea57739d80a5fdca0e29
Triet Bui Minh <x3cafe@gmail.com>
2020-02-05 04:13:01 +0700

    Add SurveyAdapter to show the list of surveys in the ViewPager

---
+++ app/src/main/java/com/myapp/ui/main/SurveyAdapter.kt
@@ -0,0 +1,52 @@
+package com.myapp.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import com.myapp.R
+import com.myapp.data.repo.SurveyItem
+import com.myapp.utils.getHighResImageUrl
+import kotlinx.android.synthetic.main.item_survey.view.*
+
+class SurveyAdapter(private val callback: MainFragment.OpenDetailCallback) :
+    RecyclerView.Adapter<PagerVH>() {
+
+    private var surveyItems = mutableListOf<SurveyItem>()
+    private var mCallback: MainFragment.OpenDetailCallback = callback
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerVH =
+        PagerVH(LayoutInflater.from(parent.context).inflate(R.layout.item_survey, parent, false))
+
+    override fun getItemCount(): Int = surveyItems.size
+
+    override fun onBindViewHolder(holder: PagerVH, position: Int) = holder.itemView.run {
+        val surveyItem = surveyItems[position]
+
+        name.text = surveyItem.title
+        description.text = surveyItem.description
+        if (surveyItem.coverImageUrl.isNotEmpty()) {
+            coverImage.load(surveyItem.getHighResImageUrl())
+        }
+        btTakeSurvey.setOnClickListener {
+            mCallback.click(surveyItem)
+        }
+    }
+
+    fun setItems(list: List<SurveyItem>) {
+        if (surveyItems.isEmpty()) {
+            this.surveyItems = list.toMutableList()
+        } else {
+            this.surveyItems.addAll(list)
+        }
+        notifyDataSetChanged()
+    }
+
+    fun clearItems() {
+        surveyItems.clear()
+        notifyDataSetChanged()
+    }
+}
+
+class PagerVH(itemView: View) : RecyclerView.ViewHolder(itemView)
