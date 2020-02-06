702a839ddcfddeb213ad537673ea98e02ad4698d
Triet Bui Minh <x3cafe@gmail.com>
2020-02-06 21:17:36 +0700

    Fix: `title` of SurveyItem cannot be null

--- app/src/main/java/com/myapp/data/repo/RepoDataModel.kt
+++ app/src/main/java/com/myapp/data/repo/RepoDataModel.kt
@@ -6,7 +6,7 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class SurveyItem(
     val id: String = "",
-    val title: String? = "",
+    val title: String = "",
     val description: String = "",
     val coverImageUrl: String = ""
 ) : Parcelable
