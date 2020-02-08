21d48c02131950531ba61d33555a00a997bb4b95
Triet Bui Minh <x3cafe@gmail.com>
2020-02-08 20:58:34 +0700

    Don't need to handle OnBackPressed in DetailFragment

--- app/src/main/java/com/myapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/myapp/ui/detail/DetailFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -33,16 +32,19 @@ class DetailFragment : DaggerFragment() {
     return inflater.inflate(R.layout.detail_fragment, container, false)
   }
 
+  @Suppress("RedundantOverride")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+    // Since we handle onBackPress in navigation_home.xml graph (app:popUpTo="@+id/mainFragment" ),
+    // don't need to manually catch for OnBackPressed here
+    /*val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
       override fun handleOnBackPressed() {
         goBack()
       }
     }
     // Let fragment handles that BackPress instead
-    requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+     requireActivity().onBackPressedDispatcher.addCallback(this, callback)*/
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
