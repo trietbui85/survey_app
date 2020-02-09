629af9e38deca5ce64901c2aeb8980cf5c4071d4
Triet Bui Minh <x3cafe@gmail.com>
2020-02-09 10:34:26 +0700

    Update test case of SurveyDataMapperTest

--- app/src/test/java/com/myapp/utils/TestData.kt
+++ app/src/test/java/com/myapp/utils/TestData.kt
@@ -3,7 +3,9 @@ package com.myapp.utils
 import com.myapp.data.local.db.AccessTokenEntity
 import com.myapp.data.local.db.SurveyEntity
 import com.myapp.data.remote.model.AccessTokenResponse
+import com.myapp.data.remote.model.SurveyResponse
 import com.myapp.data.repo.AccessTokenItem
+import com.myapp.data.repo.SurveyItem
 
 object TestData {
   private fun createSurveyEntity(index: Int) = SurveyEntity(
@@ -21,10 +23,11 @@ object TestData {
         """.trimIndent()
   }
 
-  private val surveyEntity1 = createSurveyEntity(1)
-  private val surveyEntity2 = createSurveyEntity(2)
-  private val surveyEntity3 = createSurveyEntity(3)
-  val testSurveyEntities = listOf(surveyEntity1, surveyEntity2, surveyEntity3)
+  val testSurveyEntities = listOf(
+    createSurveyEntity(1),
+    createSurveyEntity(2),
+    createSurveyEntity(3)
+  )
 
   private const val TOKEN_VALUE = "token_data"
   private const val TOKEN_TYPE = "token_type"
@@ -52,4 +55,19 @@ object TestData {
     expiresIn = EXPIRED_IN,
     createdAt = CREATED_AT
   )
+
+  private const val SURVEY_ID = "survey id"
+  private const val SURVEY_TITLE = "survey title"
+  private const val SURVEY_DESCRIPTION = "survey description"
+  private const val SURVEY_COVER_IMAGE_URL = "survey cover image url"
+
+  val testSurveyResponse = SurveyResponse(
+    SURVEY_ID, SURVEY_TITLE, SURVEY_DESCRIPTION,
+    SURVEY_COVER_IMAGE_URL, coverBackgroundColor = "#00FF00", type = "unknown type",
+    shortUrl = "bit.ly", surveyVersion = 1
+  )
+  val testSurveyEntity =
+    SurveyEntity(SURVEY_ID, SURVEY_TITLE, SURVEY_DESCRIPTION, SURVEY_COVER_IMAGE_URL)
+  val testSurveyItem =
+    SurveyItem(SURVEY_ID, SURVEY_TITLE, SURVEY_DESCRIPTION, SURVEY_COVER_IMAGE_URL)
 }
