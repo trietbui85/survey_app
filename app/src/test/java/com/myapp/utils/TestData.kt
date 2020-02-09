30f3d6cf4aaadca21a13961e9cf9d7d8717482d1
Triet Bui Minh <x3cafe@gmail.com>
2020-02-09 09:11:06 +0700

    Move all sample data (TokenEntity) for testing into TestData

--- app/src/test/java/com/myapp/utils/TestData.kt
+++ app/src/test/java/com/myapp/utils/TestData.kt
@@ -1,5 +1,6 @@
 package com.myapp.utils
 
+import com.myapp.data.local.db.AccessTokenEntity
 import com.myapp.data.local.db.SurveyEntity
 
 object TestData {
@@ -7,8 +8,24 @@ object TestData {
     "id $index", "title $index", "description $index", "image $index"
   )
 
-  val surveyEntity1 = createSurveyEntity(1)
-  val surveyEntity2 = createSurveyEntity(2)
-  val surveyEntity3 = createSurveyEntity(3)
+  private val surveyEntity1 = createSurveyEntity(1)
+  private val surveyEntity2 = createSurveyEntity(2)
+  private val surveyEntity3 = createSurveyEntity(3)
   val surveyEntities = listOf(surveyEntity1, surveyEntity2, surveyEntity3)
+
+  private const val TOKEN_VALUE = "token_data"
+  private const val TOKEN_TYPE = "token_type"
+  private const val EXPIRED_IN = 2
+  private const val CREATED_AT = 1
+
+  val jsonTokenData = """
+            {"access_token":"$TOKEN_VALUE","token_type":"$TOKEN_TYPE","expires_in":$EXPIRED_IN,"created_at":$CREATED_AT}
+        """.trimIndent()
+
+  val tokenEntity = AccessTokenEntity(
+    accessToken = TOKEN_VALUE,
+    tokenType = TOKEN_TYPE,
+    expiresIn = EXPIRED_IN,
+    createdAt = CREATED_AT
+  )
 }
