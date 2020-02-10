ffc257c7abc4f86a1fe2cd796336b4244b3b9877
Triet Bui Minh <x3cafe@gmail.com>
2020-02-11 00:51:18 +0700

    Check for endingPage, and don't need to load more once reach ending page

--- app/src/main/java/com/myapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/myapp/ui/main/MainViewModel.kt
@@ -24,7 +24,8 @@ class MainViewModel @Inject constructor(
 ) : ViewModel() {
 
   // Current page to load network data, is 0-based
-  private var _currentPage = START_PAGE_NUMBER
+  private var currentPage = START_PAGE_NUMBER
+  private var endingPage = -1
 
   // By default, use fullscreen loading in the first time
   private val _loadingFullscreenLiveData = MutableLiveData<Boolean>()
@@ -50,7 +51,7 @@ class MainViewModel @Inject constructor(
     get() = _errorLiveEvent
 
   @VisibleForTesting
-  fun getCurrentPage() = _currentPage
+  fun getCurrentPage() = currentPage
 
   fun fetchSurveys(
     pageNumber: Int = START_PAGE_NUMBER,
@@ -59,8 +60,9 @@ class MainViewModel @Inject constructor(
     Timber.d("Start fetching survey of page $pageNumber")
 
     fun fetchSurveysForPage(pageNumber: Int) {
-      _currentPage = pageNumber
-      val showFullscreenLoading = _currentPage == START_PAGE_NUMBER
+      currentPage = pageNumber
+      Timber.d("Before fetchSurveysForPage, set currentPage = $pageNumber")
+      val showFullscreenLoading = currentPage == START_PAGE_NUMBER
 
       viewModelScope.launch(mainDispatcher) {
         // Only notify change of fullscreenLoading if showFullscreenLoading is true
@@ -70,22 +72,29 @@ class MainViewModel @Inject constructor(
           _loadingMoreLiveData.value = true
         }
         val result: Result<List<SurveyItem>> =
-          surveyRepository.loadSurveys(_currentPage, numOfItemPerPage)
+          surveyRepository.loadSurveys(currentPage, numOfItemPerPage)
         if (result.status == Result.Status.SUCCESS) {
           Timber.d(
               "There are ${_contentLiveData.value?.size} existing items, " +
                   "and ${result.data?.size} new items"
           )
-          val allItems = merge2List(_contentLiveData.value, result.data)
-          (_contentLiveData.value ?: mutableListOf()).toMutableList()
-              .also {
-                it.addAll(result.data?.toMutableList().orEmpty())
-              }
-          _contentLiveData.value = allItems
+
+          if (result.data.isNullOrEmpty()) {
+            // If success but has empty list data, it means no more page to load
+            // Thus we must revert currentPage
+            currentPage--
+            endingPage = currentPage
+            Timber.d("This is the endingPage=$endingPage. No more items")
+          } else {
+            merge2List(_contentLiveData.value, result.data).let {
+              _contentLiveData.value = it
+            }
+          }
+
         } else if (result.status == Result.Status.ERROR) {
           _errorLiveEvent.value = LiveEvent(result.exception!!)
           // error means fetching is not successful, thus we must revert currentPage
-          _currentPage--
+          currentPage--
         }
 
         if (showFullscreenLoading) {
@@ -103,7 +112,7 @@ class MainViewModel @Inject constructor(
 
         fetchSurveysForPage(START_PAGE_NUMBER)
       }
-      pageNumber != _currentPage -> {
+      pageNumber != currentPage -> {
         fetchSurveysForPage(pageNumber)
       }
       else -> {
@@ -117,10 +126,16 @@ class MainViewModel @Inject constructor(
   // in progress already and we must wait for
   fun loadNextPage(): Boolean {
     if (_loadingFullscreenLiveData.value == true || _loadingMoreLiveData.value == true) {
-      Timber.d("Is still fetching data for page $_currentPage, so don't loadNext()")
+      Timber.d("Is still fetching data for page $currentPage, so don't loadNext()")
       return false
     }
-    val nextPage = _currentPage + 1
+
+    if (endingPage >= 0) {
+      Timber.d("$endingPage is the ending page. No more page to loadNext()")
+      return false
+    }
+
+    val nextPage = currentPage + 1
     Timber.d("NextPage fetching survey to page $nextPage")
     fetchSurveys(nextPage)
     return true
