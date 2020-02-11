72ce4ad5a1b5595f0d5e441a7ad73a42275a401c
Triet Bui Minh <x3cafe@gmail.com>
2020-02-11 22:13:46 +0700

    Inject account grantType, username, password to TokenRemoteDataSource

--- app/src/main/java/com/myapp/data/remote/TokenRemoteDataSource.kt
+++ app/src/main/java/com/myapp/data/remote/TokenRemoteDataSource.kt
@@ -1,6 +1,5 @@
 package com.myapp.data.remote
 
-import com.myapp.BuildConfig
 import com.myapp.data.remote.model.AccessTokenRequest
 import com.myapp.data.remote.model.AccessTokenResponse
 import timber.log.Timber
@@ -12,12 +11,14 @@ interface TokenRemoteDataSource {
 
 class TokenRemoteDataSourceImpl(
   private val tokenApiService: TokenApiService,
-  private val defaultTokenRequest: AccessTokenRequest = AccessTokenRequest(
-      grantType = BuildConfig.ACCOUNT_GRANT_TYPE,
-      username = BuildConfig.ACCOUNT_DEFAULT_USERNAME,
-      password = BuildConfig.ACCOUNT_DEFAULT_PASSWORD
-  )
+  grantType: String,
+  username: String,
+  password: String
 ) : TokenRemoteDataSource {
+  private val defaultTokenRequest: AccessTokenRequest = AccessTokenRequest(
+    grantType, username, password
+  )
+
   override suspend fun refreshAccessToken(): AccessTokenResponse {
     Timber.d("defaultTokenRequest = $defaultTokenRequest")
     return tokenApiService.refreshAccessToken(defaultTokenRequest)
