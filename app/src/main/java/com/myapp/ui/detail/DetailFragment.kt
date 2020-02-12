56f32192e2b19cf4a0d85ce8e73b0925c4f534c1
Triet Bui Minh <x3cafe@gmail.com>
2020-02-13 01:32:03 +0700

    Remove redundant `onCreate` method in DetailFragment

--- app/src/main/java/com/myapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/myapp/ui/detail/DetailFragment.kt
@@ -32,21 +32,6 @@ class DetailFragment : DaggerFragment() {
     return inflater.inflate(R.layout.detail_fragment, container, false)
   }
 
-  @Suppress("RedundantOverride")
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    // Since we handle onBackPress in navigation_home.xml graph (app:popUpTo="@+id/mainFragment" ),
-    // don't need to manually catch for OnBackPressed here
-    /*val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
-      override fun handleOnBackPressed() {
-        goBack()
-      }
-    }
-    // Let fragment handles that BackPress instead
-     requireActivity().onBackPressedDispatcher.addCallback(this, callback)*/
-  }
-
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
 
